ca99516cdf0e1711e9ccb627664110fdd240dbe7
Gavaskar <mgavaskar24@gmail.com>
2023-09-11 20:58:27 +0530

    Done problem 2a

---
+++ README.md
@@ -0,0 +1,10 @@
+# DS211
+Assignment 1
+Ways to run my code
+COMPILE: g++ test.cpp
+RUN:
+    g++ -o myprogram.exe test.cpp
+    g++ -o myprogram test.cpp
+    ./myprogram input.txt output.txt
+
+
--- user_code.h
+++ user_code.h
@@ -1,70 +1,86 @@
-#ifndef USER_CODE_H
-#define USER_CODE_H
-
-#include <vector>
-#include <string>
-#include <utility>
-#include <unordered_map>
-
-// Function to convert a dense matrix to a sparse matrix representation
-void denseToSparse(const std::vector<std::vector<int> >& denseMatrix,
-                   std::vector<int>& values,
-                   std::vector<int>& columnIndices,
-                   std::vector<int>& rowPointers){
-                    // Write your code here
-                    
-                    return;
+// Problem 1
+// A function to convert a dense matrix to a sparse matrix
+//  Input: denseMatrix - a 2D array of integers
+//  Output: values - a 1D array of integers
+//         columnIndices - a 1D array of integers
+//          rowPointers - a 1D array of integers
+//         (see the assignment description for details)
+void denseToSparse(const std::vector<std::vector<int>>& denseMatrix, std::vector<int>& values, std::vector<int>& columnIndices, std::vector<int>& rowPointers) {
+   int n = denseMatrix.size();
+   int m = denseMatrix[0].size();
+   int count = 0;
+   for(int i = 0; i < n; i++) {
+       rowPointers.push_back(count);
+       for(int j = 0; j < m; j++) {
+           if(denseMatrix[i][j] != 0) {
+               values.push_back(denseMatrix[i][j]);
+               columnIndices.push_back(j);
+               count++;
+           }
+       }
+   }
+   rowPointers.push_back(count);
 }
 
 
-//Function to perform dense matrix multiplication
 
-void denseMatrixMultiplication(const std::vector<std::vector<int> >& denseMatrix1,
-								const std::vector<std::vector<int> >& denseMatrix2,
-                                int squareMatrixSize,
-                                std::vector<std::vector<int> >& denseMatrix_result){
-                                // Write your code here
-                                return;
+// Problem 2(a)
+
+//  Given two square dense matrices of sizes N, Find their matrix multiplication and store it in denseMatrix
+//  Input: denseMatrix1 - a 2D array of integers
+//         denseMatrix2 - a 2D array of integers
+//         N - size of the matrices
+//  Output: denseMatrix - a 2D array of integers
+//         (see the assignment description for details)
+void denseMatrixMultiplication(const std::vector<std::vector<int>>& denseMatrix1, const std::vector<std::vector<int>>& denseMatrix2,int N, 
+                    std::vector<std::vector<int>>& denseMatrix) 
+                    {
+                        for(int i = 0; i < N; i++) {
+                            std::vector<int> temp;
+                            for(int j = 0; j < N; j++) {
+                                int sum = 0;
+                                for(int k = 0; k < N; k++) {
+                                    sum += denseMatrix1[i][k] * denseMatrix2[k][j];
+                                }
+                                temp.push_back(sum);
+                            }
+                            denseMatrix.push_back(temp);
+                        }
+
+                    }
+
+// Problem 2(b)
+
+// Given two dense square matrices of size N, perform block Matrix-matrix with a block size B and store it in denseMatrix
+//  Input: denseMatrix1 - a 2D array of integers
+//         denseMatrix2 - a 2D array of integers
+//         N - size of the matrices
+//         B - size of the block
+//  Output: denseMatrix - a 2D array of integers
+//         (see the assignment description for details)
+
+void blockMatrixMultiplication(const std::vector<std::vector<int>>& denseMatrix1,
+								const std::vector<std::vector<int>>& denseMatrix2,
+								int N, int B,
+								const std::vector<std::vector<int>>& denseMatrix_result){
+                                    int n = N/B;
+                                    for(int i = 0; i < n; i++){
+                                        for(int j = 0; j < n; j++){
+                                            for(int k = 0; k < n; k++){
+                                                for(int i1 = i*B; i1 < (i+1)*B; i1++){
+                                                    for(int j1 = j*B; j1 < (j+1)*B; j1++){
+                                                        for(int k1 = k*B; k1 < (k+1)*B; k1++){
+                                                            denseMatrix_result[i1][j1] += denseMatrix1[i1][k1] * denseMatrix2[k1][j1];
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
 
 
-//Function to perform dense matrix multiplication using block matrix multiplication
-void blockMatrixMultiplication(const std::vector<std::vector<int> >& denseMatrix1,
-								const std::vector<std::vector<int> >& denseMatrix2,
-                                int N, int B,
-                                std::vector<std::vector<int> >& denseMatrix_result){
-                                // Write your code here
-                                return;
-                            }
 
-// Function to perform sparse matrix multiplication
-void sparseMatrixMultiplication(const std::vector<int>& values1,
-                                const std::vector<int>& columnIndices1,
-                                const std::vector<int>& rowPointers1,
-                                const std::vector<int>& values2,
-                                const std::vector<int>& columnIndices2,
-                                const std::vector<int>& rowPointers2,
-                                int N,
-                                std::vector<int>& valuesResult,
-                                std::vector<int>& columnIndicesResult,
-                                std::vector<int>& rowPointersResult){
-                                // Write your code here
-                                return;
-}
+            
 
 
-//function For finding patterns in graphs using a map
-void findTrianglesAsMap(const std::vector<std::vector<int> >& adjMat, int N, std::map<std::vector<int>, int> outputMap){
-    // Write your code here
-    return;
-}
-
-
-//function For finding patterns in graphs using a vector
-
-void findTrianglesAsVec(const std::vector<std::vector<int> >& adjMat, int N,  std::vector<std::vector<int> >& outputVec){
-    // Write your code here
-    return;
-}
-
-#endif // USER_CODE_H
