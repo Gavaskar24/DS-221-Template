dff22bfb5ece88325d2d71934539d62111b80fed
Gavaskar <mgavaskar24@gmail.com>
2023-09-15 22:08:39 +0530

    save

--- main.cpp
+++ main.cpp
@@ -294,28 +294,28 @@ int problem3(string inputPath, string outputPath){
 
 
 int main(int argc, char** argv) {
-    string problem1InputPath = argv[1];
-    string problem1OutputPath = argv[2];
-    string problem2aInputPath = argv[3];
-    string problem2aOutputPath = argv[4];
-    string problem2bInputPath = argv[5];
-    string problem2bOutputPath = argv[6];
-    string problem2cInputPath = argv[7];
-    string problem2cOutputPath = argv[8];
-    string problem3InputPath = argv[9];
-    string problem3OutputPath = argv[10];
+    // string problem1InputPath = argv[1];
+    // string problem1OutputPath = argv[2];
+    string problem2aInputPath = argv[1];
+    string problem2aOutputPath = argv[2];
+    // string problem2bInputPath = argv[5];
+    // string problem2bOutputPath = argv[6];
+    // string problem2cInputPath = argv[7];
+    // string problem2cOutputPath = argv[8];
+    // string problem3InputPath = argv[9];
+    // string problem3OutputPath = argv[10];
    
 
     // Calling user code for Problem 1
     int p1Ret, p2aRet,p2bRet,p2cRet, p3Ret;
 
-    cout << "Problem 1" << endl;
-    p1Ret = problem1(problem1InputPath, problem1OutputPath);
-    if(p1Ret != 0){
+    // cout << "Problem 1" << endl;
+    // p1Ret = problem1(problem1InputPath, problem1OutputPath);
+    // if(p1Ret != 0){
 
-        cout << "Error in problem 1" << endl;
-    }
-    cout << "----------------------------" << endl;
+    //     cout << "Error in problem 1" << endl;
+    // }
+    // cout << "----------------------------" << endl;
 
     cout << "Problem 2a" << endl;
     p2aRet = problem2a(problem2aInputPath, problem2aOutputPath);
@@ -324,24 +324,24 @@ int main(int argc, char** argv) {
     }
     cout << "----------------------------" << endl;
 
-    cout << "Problem 2b" << endl;
-    p2bRet = problem2b(problem2bInputPath, problem2bOutputPath);
-    if(p2bRet != 0){
-      cout << "Error in problem 2b" << endl;
-    }
-    cout << "----------------------------" << endl;
+    // cout << "Problem 2b" << endl;
+    // p2bRet = problem2b(problem2bInputPath, problem2bOutputPath);
+    // if(p2bRet != 0){
+    //   cout << "Error in problem 2b" << endl;
+    // }
+    // cout << "----------------------------" << endl;
 
-    cout << "Problem 2c" << endl;
-    p2cRet = problem2c(problem2cInputPath, problem2cOutputPath);
-    if(p2cRet != 0){
-      cout << "Error in problem 2c" << endl;
-    }
-    cout << "----------------------------" << endl;
+    // cout << "Problem 2c" << endl;
+    // p2cRet = problem2c(problem2cInputPath, problem2cOutputPath);
+    // if(p2cRet != 0){
+    //   cout << "Error in problem 2c" << endl;
+    // }
+    // cout << "----------------------------" << endl;
 
-    cout << "Problem 3" << endl;
-    p3Ret = problem3(problem3InputPath, problem3OutputPath);
-    if(p3Ret != 0){
-      cout << "Error in problem 3" << endl;
-    }
-    cout << "----------------------------" << endl;
+    // cout << "Problem 3" << endl;
+    // p3Ret = problem3(problem3InputPath, problem3OutputPath);
+    // if(p3Ret != 0){
+    //   cout << "Error in problem 3" << endl;
+    // }
+    // cout << "----------------------------" << endl;
 }
--- user_code.h
+++ user_code.h
@@ -1,26 +1,35 @@
 // Problem 1
-// A function to convert a dense matrix to a sparse matrix
-//  Input: denseMatrix - a 2D array of integers
-//  Output: values - a 1D array of integers
-//         columnIndices - a 1D array of integers
-//          rowPointers - a 1D array of integers
-//         (see the assignment description for details)
-void denseToSparse(const std::vector<std::vector<int>>& denseMatrix, std::vector<int>& values, std::vector<int>& columnIndices, std::vector<int>& rowPointers) {
-   int n = denseMatrix.size();
-   int m = denseMatrix[0].size();
-   int count = 0;
-   for(int i = 0; i < n; i++) {
-       rowPointers.push_back(count);
-       for(int j = 0; j < m; j++) {
-           if(denseMatrix[i][j] != 0) {
-               values.push_back(denseMatrix[i][j]);
-               columnIndices.push_back(j);
-               count++;
-           }
-       }
-   }
-   rowPointers.push_back(count);
+// Convert a dense matrix to compressed sparse row format
+// where each rowpointer is the index of the first non-zero element in that row and is offset by 0 at the beginning
+#include<iostream>
+#include<vector>
+#include<fstream>
+
+using namespace std;
+
+void denseToSparse(const std::vector<std::vector<int>>& denseMatrix,
+                   std::vector<int>& values,
+                   std::vector<int>& columnIndices,
+                   std::vector<int>& rowPointers){
+    int n = denseMatrix.size();
+    int m = denseMatrix[0].size();
+    int count = 0;
+    
+    for(int i = 0; i < n; i++){
+      
+        rowPointers.push_back(count);
+        for(int j = 0; j < m; j++){
+            if(denseMatrix[i][j] != 0){
+                values.push_back(denseMatrix[i][j]);
+                columnIndices.push_back(j);
+                count++;
+                   }
+        }
+        
+    }
+    rowPointers.push_back(count);
 }
+
 
 
 
@@ -32,22 +41,30 @@ void denseToSparse(const std::vector<std::vector<int>>& denseMatrix, std::vector
 //         N - size of the matrices
 //  Output: denseMatrix - a 2D array of integers
 //         (see the assignment description for details)
-void denseMatrixMultiplication(const std::vector<std::vector<int>>& denseMatrix1, const std::vector<std::vector<int>>& denseMatrix2,int N, 
-                    std::vector<std::vector<int>>& denseMatrix) 
-                    {
-                        for(int i = 0; i < N; i++) {
-                            std::vector<int> temp;
-                            for(int j = 0; j < N; j++) {
-                                int sum = 0;
-                                for(int k = 0; k < N; k++) {
-                                    sum += denseMatrix1[i][k] * denseMatrix2[k][j];
-                                }
-                                temp.push_back(sum);
-                            }
-                            denseMatrix.push_back(temp);
-                        }
 
-                    }
+void denseMatrixMultiplication(const std::vector<std::vector<int> >& denseMatrix1,
+                               const std::vector<std::vector<int> >& denseMatrix2,
+                               int N,
+                              const std::vector<std::vector<int> >& denseMatrix_result){
+
+                                
+
+                                for(int i=0;i<N;i++){
+                                    for(int j=0;j<N;j++){
+                                       
+                                        for(int k=0;k<N;k++){
+                                            // denseMatrix_result[i][j]+=denseMatrix1[i][k]*denseMatrix2[k][j];
+                                            cout<<denseMatrix1[i][k]*denseMatrix2[k][j]<<" ";
+                                           
+                                        }
+                                        cout<<endl;
+                                        // cout<<denseMatrix_result[i][j]<<" ";
+                                    }
+                                    // cout<<endl;
+                                }
+                                return;
+                            }
+
 
 // Problem 2(b)
 
@@ -62,7 +79,8 @@ void denseMatrixMultiplication(const std::vector<std::vector<int>>& denseMatrix1
 void blockMatrixMultiplication(const std::vector<std::vector<int>>& denseMatrix1,
 								const std::vector<std::vector<int>>& denseMatrix2,
 								int N, int B,
-								const std::vector<std::vector<int>>& denseMatrix_result){
+								 std::vector<std::vector<int>>& denseMatrixResult){
+                                    std::vector<std::vector<int>> temp(N, std::vector<int>(N, 0));
                                     int n = N/B;
                                     for(int i = 0; i < n; i++){
                                         for(int j = 0; j < n; j++){
@@ -70,15 +88,44 @@ void blockMatrixMultiplication(const std::vector<std::vector<int>>& denseMatrix1
                                                 for(int i1 = i*B; i1 < (i+1)*B; i1++){
                                                     for(int j1 = j*B; j1 < (j+1)*B; j1++){
                                                         for(int k1 = k*B; k1 < (k+1)*B; k1++){
-                                                            denseMatrix_result[i1][j1] += denseMatrix1[i1][k1] * denseMatrix2[k1][j1];
+                                                            temp[i1][j1] += denseMatrix1[i1][k1] * denseMatrix2[k1][j1];
                                                         }
                                                     }
                                                 }
                                             }
                                         }
                                     }
+                                    denseMatrixResult=temp;
                                 }
 
+void sparseMatrixMultiplication(const std::vector<int>& values1,
+                                const std::vector<int>& columnIndices1,
+                                const std::vector<int>& rowPointers1,
+                                const std::vector<int>& values2,
+                                const std::vector<int>& columnIndices2,
+                                const std::vector<int>& rowPointers2,
+                                int N,
+                                std::vector<int>& valuesResult,
+                                std::vector<int>& columnIndicesResult,
+                                std::vector<int>& rowPointersResult){
+                                // Write your code here
+                                return;
+}
+
+
+//function For finding patterns in graphs using a map
+void findTrianglesAsMap(const std::vector<std::vector<int> >& adjMat, int N, std::map<std::vector<int>, int> outputMap){
+    // Write your code here
+    return;
+}
+
+
+//function For finding patterns in graphs using a vector
+
+void findTrianglesAsVec(const std::vector<std::vector<int> >& adjMat, int N,  std::vector<std::vector<int> >& outputVec){
+    // Write your code here
+    return;
+}
 
 
             
